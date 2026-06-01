53ab1b188f9a7e9e6dccff8c0b36e3dae420f0ee
Siphosihle Sehume <siphosihlesehume@gmail.com>
2026-06-01 17:18:32 +0200

    ADD: implemented core logic

--- src/main/java/za/co/eliteproject/enums/AssetType.java
+++ src/main/java/za/co/eliteproject/enums/AssetType.java
@@ -1,5 +1,16 @@
 package za.co.eliteproject.enums;
 
 public enum AssetType {
+    public String label;
 
+    ELECTRICITY, GAS, CARBON, COAL;
+
+
+    public AssetType(String label) {
+        this.label = getLabel();
+    }
+
+    public AssetType getLabel() {
+        //
+    }
 }
