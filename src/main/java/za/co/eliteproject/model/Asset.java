63ac76e6481a383613dcf614ed385f486cdc9363
Siphosihle Sehume <siphosihlesehume@gmail.com>
2026-06-01 17:15:49 +0200

    ADD: implemented core logic
    Fix method signature and logic errors in Asset class.

--- src/main/java/za/co/eliteproject/model/Asset.java
+++ src/main/java/za/co/eliteproject/model/Asset.java
@@ -1,4 +1,51 @@
 package za.co.eliteproject.model;
 
 public class Asset {
+    private AssetType assetType;
+    private double quantity;
+    private double avgCost;
+
+    public Asset(AssetType assetType, double quantity, double avgCost) {
+        this.assetType = assetType;
+        this.quantity = quantity;
+        this.avgCost = avgCost;
+    }
+
+    public AssetType getAssetType() {
+        return assetType;
+    }
+
+    public double getQuanity() {
+        return quantity;
+    }
+
+    public double getAvgCost() {
+        return avgCost;
+    }
+
+    public void add(double quant, double price) {
+        this.quantity += quant;
+        avgCost = ((this.quantity * this.quantity) + (quant * price))
+             / (this.quantity + quant);
+    }
+
+    public boolean reduce(double quant) {
+        quantity -= quant;
+        if (quantity <= 0) {
+            return false;
+        }
+        return false;
+    }
+
+    public double getMarketValue(double currentPrice) {
+        return quantity * currentPrice;
+    } 
+
+    public getUnrealisedPnL(double currentPrice) {
+        return (currentPrice * avgCost) * quantity;
+    }
+
+    @Override String toString() {
+        // Generate using IntelliJ
+    }
 }
