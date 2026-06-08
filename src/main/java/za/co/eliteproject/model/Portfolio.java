c0ba6f433e934fb8ecc1ea017c876c6d5c7ee156
Siphosihle Sehume <lukelevi63@gmail.com>
2026-06-08 14:45:02 +0200

    ADD: Portfolio

--- src/main/java/za/co/eliteproject/model/Portfolio.java
+++ src/main/java/za/co/eliteproject/model/Portfolio.java
@@ -3,28 +3,26 @@ package za.co.eliteproject.model;
 import za.co.eliteproject.enums.AssetType;
 
 import java.util.*;
+import java.util.concurrent.RecursiveTask;
 
 public class Portfolio {
     private int portfolioId;
     private double cashBalance;
-    private double reversedCash;
+    private double reservedCash;
     private HashMap<AssetType, Asset> assets;
     private double realisedPnL;
 
     public Portfolio(int portfolioId, double initialCash) {
-        this.porfolioId = portfolioId;
-        this.initialCash = initialCash;
+        this.portfolioId = portfolioId;
+        this.cashBalance = initialCash;
+        this.assets = new HashMap<>();
     }
 
     public int getPortfolioId() {
-        return porfolioId;
+        return portfolioId;
     }
 
     public double getCashBalance() {
-        return cashBalance;
-    }
-
-    public double getReservedCash() {
         return reservedCash;
     }
 
@@ -32,42 +30,53 @@ public class Portfolio {
         return cashBalance + reservedCash;
     }
 
-    public boolean reservedCash(double amount) {
-        cashBalance -= amount;
-        reservedCash += amount;
-        
-        if (cashBalance <= 0) {
-            return false;
+    public boolean reserveCash(double amount) {
+        if (cashBalance > 0) {
+            cashBalance -= amount;
+            reservedCash += amount;
+            return true;
         }
-
-        return true;
-    }
-
-    public void releaseCash(double amount) {
-        reservedCash -= amount;
-        cashBalance += amount;
+        return false;
     }
 
     public boolean deductReservedCash(double amount) {
-        reservedCash -= amount;
-
-        if (reservedCash <= 0) {
-            return false;
+        if (reservedCash > 0) {
+            reservedCash -= amount;
+            return true;
         }
-
-        return true;
+        return false;
     }
 
-    public Asset getAset(AssetType asset) {
-        Asset asset = assets.getOrDefault(asset, null);
-        return asset;
+    public void creditCash(double amount) {
+        cashBalance += amount;
+    }
+
+    public void addAsset(AssetType type, double qty, double price) {
+        if (assets.containsKey(type)) {
+            assets.get(type).add(qty, price); //By getting the type you retrieve the value
+        }
+        else {
+              assets.put(type, new Asset(type, qty, price));
+        }
+    }
+
+    public boolean reduceAsset(AssetType type, double qty) {
+        if (assets.containsKey(type)) {
+            assets.get(type).reduce(qty);
+            return true;
+        }
+        return false;
+     }
+
+    public Asset getAsset(AssetType type) {
+        return assets.get(type);
     }
 
     public Map<AssetType, Asset> getAllAssets() {
-        return new ArrayList<>(assets);
+        return new HashMap<>(assets);
     }
 
-    public double getRealizedPnL(double amount) {
+    public double getRealizedPnL() {
         return realisedPnL;
     }
 
@@ -75,17 +84,25 @@ public class Portfolio {
         realisedPnL += amount;
     }
 
-    public double getTotalValue(Map<AssetType, Double> prices) {
-        Double asset = null;
+    public double getTotalValue(Map<AssetType,Double> prices) {
+        double totalPrice = getTotalCash();
 
         for (Map.Entry<AssetType, Double> entry : prices.entrySet()) {
-            Double asset = entry.getValue();
+            AssetType type = entry.getKey();
+            double price = entry.getValue();
+
+            if (prices != null && prices,)
         }
-        return asset;
     }
 
     @Override
     public String toString() {
-        ///
+        return "Portfolio{" +
+                "portfolioId=" + portfolioId +
+                ", cashBalance=" + cashBalance +
+                ", reservedCash=" + reservedCash +
+                ", assets=" + assets +
+                ", realisedPnL=" + realisedPnL +
+                '}';
     }
 }
