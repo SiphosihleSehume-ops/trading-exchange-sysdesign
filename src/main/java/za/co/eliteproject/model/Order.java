b065640a55d34281809d7a6e9b03962e5d6aa2b6
Siphosihle Sehume <siphosihlesehume@gmail.com>
2026-06-13 16:08:57 +0200

    ADD: constructor to Order

--- src/main/java/za/co/eliteproject/model/Order.java
+++ src/main/java/za/co/eliteproject/model/Order.java
@@ -9,4 +9,16 @@ public class Order {
   protected double filledQty;
   protected OrderStatus status;
   protected long timestamp;
+
+  protected Order(int orderId, Trader trader, AssetType assetType,
+                  OrderSide order, double quantity) {
+    this.orderId = orderId;
+    this.trader = trader;
+    this.assetType = assetType;
+    this.order = order;
+    this.quantity = quantity;
+    this.filledQty = 0;
+    this.status = OrderStatus.PENDING;
+    this.timestamp = System.currentTimeMillis();
+  }
 }
