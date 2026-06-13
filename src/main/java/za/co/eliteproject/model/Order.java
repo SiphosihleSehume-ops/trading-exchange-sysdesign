b6f46394c5cff508226dc82d0e8b5cd1e322e21c
Siphosihle Sehume <siphosihlesehume@gmail.com>
2026-06-13 15:56:24 +0200

    ADD: fields to Order class

--- src/main/java/za/co/eliteproject/model/Order.java
+++ src/main/java/za/co/eliteproject/model/Order.java
@@ -1,4 +1,12 @@
 package za.co.eliteproject.model;
 
 public class Order {
+  protected int orderId;
+  protected Trader trader;
+  protected AssetType assetType;
+  protected OrderSide order;
+  protected double quantity;
+  protected double filledQty;
+  protected OrderStatus status;
+  protected long timestamp;
 }
