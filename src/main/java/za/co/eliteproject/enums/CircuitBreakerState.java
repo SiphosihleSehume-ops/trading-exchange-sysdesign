9fa60885db7579258b5c052d7811a4af5c2daf26
Siphosihle Sehume <siphosihlesehume@gmail.com>
2026-07-06 20:11:02 +0200

    ADD: fields to Order class
    Added 'allowsTrading' method to determine trading state based on CircuitBreakerState.

--- src/main/java/za/co/eliteproject/enums/CircuitBreakerState.java
+++ src/main/java/za/co/eliteproject/enums/CircuitBreakerState.java
@@ -1,4 +1,17 @@
 package za.co.eliteproject.enums;
 
 public enum CircuitBreakerState {
+    CLOSED, OPEN, TESTING;
+
+    public boolean allowsTrading() {
+        switch (this) {
+            // Chain similar logic
+            case CLOSED:
+            case TESTING:
+                return true;
+            case OPEN:
+                return false;
+        }
+        return false;
+    }
 }
