084aa022c333dcea1c95120baa8e344b54b93ff8
Siphosihle Sehume <siphosihlesehume@gmail.com>
2026-08-23 20:38:00 +0200

    ADD: risk rule

--- src/main/java/za/co/eliteproject/risk/RiskRule.java
+++ src/main/java/za/co/eliteproject/risk/RiskRule.java
@@ -1,4 +1,18 @@
+
 package za.co.eliteproject.risk;
 
-public class RiskRule {
+import za.co.eliteproject.model.Market;
+import za.co.eliteproject.model.Order;
+
+/**
+ * A single rule in the RiskEngine's chain of responsibility.
+ */
+public interface RiskRule {
+
+    /**
+     * @return true if the order passes the rule, false if it should be rejected.
+     */
+    boolean evaluate(Order order, Market market);
+
+    String getRuleName();
 }
